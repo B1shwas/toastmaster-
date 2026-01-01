bce210532ea080e35ecb8b4c4ddb8d5b27d327a3
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2026-01-01 21:12:49 +0545

    animation and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,41 @@ module.exports = {
   ],
   theme: {
     extend: {
+      transitionTimingFunction: {
+        "minor-spring": "cubic-bezier(0.18,0.89,0.82,1.04)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+        // wave reveal
+        "reveal-up": {
+          "0%": { opacity: "0", transform: "translateY(80%)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "reveal-down": {
+          "0%": { opacity: "0", transform: "translateY(-80%)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "content-blur": {
+          "0%": { filter: "blur(0.3rem)" },
+          "100%": { filter: "blur(0)" },
+        },
+      },
+
       colors: {
         brand: {
           blue: "#3B82F6",
@@ -44,7 +79,12 @@ module.exports = {
         xs: "2px",
         sm: "4px",
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+
     },
   },
-  plugins: [],
+  plugins: [tailwindAnimate],
 };
